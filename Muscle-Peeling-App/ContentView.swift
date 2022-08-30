f19906f1a31b628e2cca1bd9ed30d3c0fa8a9640
kazua33 <88267594+kazua33@users.noreply.github.com>
2022-08-30 11:18:36 +0900

    プロジェクト作成

---
+++ Muscle-Peeling-App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Muscle-Peeling-App
+//
+//  Created by 稗田一亜 on 2022/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+
+            Text("Hello, moーーーki!")
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Muscle-Peeling-App/Muscle_Peeling_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Muscle_Peeling_AppApp.swift
+//  Muscle-Peeling-App
+//
+//  Created by 稗田一亜 on 2022/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Muscle_Peeling_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
