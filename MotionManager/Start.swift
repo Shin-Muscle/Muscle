01f7e387937f4af41329f28595e7a46fa0ac833b
05inagaki <94812178+inagaki05@users.noreply.github.com>
2022-09-16 10:13:24 +0900

    view

---
+++ MotionManager/Start.swift
@@ -0,0 +1,20 @@
+//
+//  Start.swift
+//  MotionManager
+//
+//  Created by cmStudent on 2022/09/13.
+//
+
+import SwiftUI
+
+struct Start: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Start_Previews: PreviewProvider {
+    static var previews: some View {
+        Start()
+    }
+}
---
+++ MotionManager/syoki.swift
@@ -0,0 +1,20 @@
+//
+//  syoki.swift
+//  MotionManager
+//
+//  Created by cmStudent on 2022/09/16.
+//
+
+import SwiftUI
+
+struct syoki: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct syoki_Previews: PreviewProvider {
+    static var previews: some View {
+        syoki()
+    }
+}
